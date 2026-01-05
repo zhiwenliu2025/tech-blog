da25d1ef4b941828737ba486b3d9a3a257e3875d
georgeliu <16445730+georgeliu1122@user.noreply.gitee.com>
2026-01-05 15:42:46 +0800

    refactor(博客页面): 将作者链接从NuxtLink改为按钮样式
    修改作者链接的交互方式，使用标准按钮元素替代NuxtLink组件，并更新样式以符合设计规范

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -75,14 +75,15 @@
                 </p>
               </div>
             </div>
-            <NuxtLink
+            <button
               v-if="post?.author_id"
-              :to="`/authors/${post.author_id}`"
-              class="inline-flex items-center space-x-1 rounded-md border border-blue-600 bg-white px-3 py-1.5 text-sm font-medium text-blue-600 transition-colors hover:bg-blue-50 dark:border-blue-400 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
+              type="button"
+              class="inline-flex items-center space-x-1 rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:hover:bg-blue-600"
+              @click="navigateTo(`/authors/${post.author_id}`)"
             >
               <span>查看作者文章</span>
               <Icon name="heroicons:arrow-right" class="h-4 w-4" />
-            </NuxtLink>
+            </button>
           </div>
 
           <!-- 标签 -->
