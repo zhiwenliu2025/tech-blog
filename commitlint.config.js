c2e6b03791db0b4a55a0f4492f29215492d44aae
georgeliu <16445730+georgeliu1122@user.noreply.gitee.com>
2026-02-10 14:40:42 +0800

    chore(commitlint): update configuration to increase line length limits
    - Modified commitlint rules to allow a maximum header length of 200 characters.
    - Updated body and footer line length limits to 200 characters for better flexibility in commit messages.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 export default {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // 修改 header（第一行）最大长度，默认是 100
+    'header-max-length': [2, 'always', 200],
+    // 修改 body 每行最大长度，默认是 100
+    'body-max-line-length': [2, 'always', 200],
+    // 修改 footer 每行最大长度，默认是 100
+    'footer-max-line-length': [2, 'always', 200]
+  }
 }
