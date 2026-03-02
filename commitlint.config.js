9851be88ff64c8df3065293d53b2dddce2046894
georgeliu <16445730+georgeliu1122@user.noreply.gitee.com>
2026-03-02 16:36:17 +0800

    chore(commitlint): increase maximum line length for header, body, and footer to 400 characters

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,10 +2,10 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     // 修改 header（第一行）最大长度，默认是 100
-    'header-max-length': [2, 'always', 200],
+    'header-max-length': [2, 'always', 400],
     // 修改 body 每行最大长度，默认是 100
-    'body-max-line-length': [2, 'always', 200],
+    'body-max-line-length': [2, 'always', 400],
     // 修改 footer 每行最大长度，默认是 100
-    'footer-max-line-length': [2, 'always', 200]
+    'footer-max-line-length': [2, 'always', 400]
   }
 }
